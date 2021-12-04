ffc87dd69529eab13aa9ead53ad04bc356b4a78c
Leonardo <leonardovinicius_silva@hotmail.com>
2021-12-04 16:19:48 -0300

    Adicionado um repositorio local com o SharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:crypto_coins/configs/app_settings.dart';
-import 'package:crypto_coins/configs/hive_configs.dart';
 import 'package:crypto_coins/meu_aplicativo.dart';
 import 'package:crypto_coins/repositories/favoritas_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await HiveConfig.start();
+void main() {
 
   runApp(
     MultiProvider(
